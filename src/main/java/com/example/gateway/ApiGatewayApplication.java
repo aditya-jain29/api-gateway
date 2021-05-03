1ff59ea6d26003c531c55d61a66f56d1554a2932
aditya <er.adityajain1989@gmail.com>
2021-05-03 13:46:43 -0500

    initial commit

---
+++ src/main/java/com/example/gateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.codec.ServerCodecConfigurer;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
